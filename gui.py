ad28c30fb91e0a36e0be4b356db36b39360f9a30
Duc <phanducckd@gmail.com>
2022-11-29 12:57:35 +0700

    Implement all function

--- gui.py
+++ gui.py
@@ -8,8 +8,13 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_data(), key="list",
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
-window = sg.Window("My Adding App", layout=[[label], [input_box, add_button],
-                                            [list_box, edit_button]])
+delete_button = sg.Button("Delete")
+exit_button = sg.Button("Exit")
+window = sg.Window("My Adding App",
+                   layout=[[label],
+                        [input_box, add_button],
+                        [list_box, edit_button, delete_button],
+                           [exit_button]])
 
 while True:
     event, value = window.read()
@@ -23,7 +28,7 @@ while True:
             data.append(value["to_do"] + "\n")
             function.write_data(data)
             window["list"].update(values=data)
-
+            window["to_do"].update(value="")
         case "Edit":
             todo_edit = value["list"][0]
             new_todo = value["to_do"]
@@ -34,8 +39,17 @@ while True:
             data[index] = new_todo
             function.write_data(data)
             window["list"].update(values=data)
+        case "Delete":
+            todo_delete = value["list"][0]
+            data = function.get_data()
+            data.remove(todo_delete)
+            function.write_data(data)
+            window["list"].update(values=data)
+            window["to_do"].update(value="")
+        case "Exit":
+           break
         case "list":
-            window["to_do"].update(value=value['list'][0])
+            window["to_do"].update(value["list"][0])
 
 
         case sg.WIN_CLOSED:
